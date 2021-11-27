45d011ff5397179da807b8b602c9a7d750ded03d
mohamed3asem3 <m.abdallah@techsquadeg.com>
2021-11-27 19:53:09 +0200

    add memoization for chart container

--- src/components/ChartBoxContainer/LineChartConainer/LineChartContainer.js
+++ src/components/ChartBoxContainer/LineChartConainer/LineChartContainer.js
@@ -1,39 +1,44 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, memo } from 'react';
 
 import ErrorBoundry from 'src/components/ErrorBounderies/ErrorBoundary';
 import { LineChart } from 'src/components/Charts/LineChart/LineChart';
 import { useFetchData } from 'src/hooks/fetchData';
 import { formulateGraphData } from 'src/utils/helperFunctions';
 
-export const LineChartContainer = ({ dimension, measures }) => {
-  const [chartData, setChartData] = useState([]);
+export const LineChartContainer = memo(
+  ({ dimension, measures }) => {
+    const [chartData, setChartData] = useState([]);
 
-  const rawData = useFetchData(
-    'data',
-    'post',
-    [dimension, measures, 'post', 'data'],
-    {
-      measures,
-      dimension,
-    }
-  );
+    const rawData = useFetchData(
+      'data',
+      'post',
+      [dimension, measures, 'post', 'data'],
+      {
+        measures,
+        dimension,
+      }
+    );
 
-  useEffect(() => {
-    if (rawData) {
-      const chartData = formulateGraphData(rawData, dimension);
-      setChartData(chartData);
-    }
-  }, [rawData]);
+    useEffect(() => {
+      if (rawData) {
+        const chartData = formulateGraphData(rawData, dimension);
+        setChartData(chartData);
+      }
+    }, [rawData]);
 
-  if (!chartData.length || !dimension || !measures.length) return null;
+    if (!chartData.length || !dimension || !measures.length) return null;
 
-  return (
-    <ErrorBoundry>
-      <LineChart
-        chartData={chartData}
-        dimension={dimension}
-        measures={measures}
-      />
-    </ErrorBoundry>
-  );
-};
+    return (
+      <ErrorBoundry>
+        <LineChart
+          chartData={chartData}
+          dimension={dimension}
+          measures={measures}
+        />
+      </ErrorBoundry>
+    );
+  },
+  (prevProps, nextProps) => {
+    if (!nextProps.dimension || !nextProps.measures.length) return true;
+  }
+);
